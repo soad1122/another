a27c6efe8b5f1ffc7a3fd89e040c25e973fc9af0
soad1122 <souaduzzaman234@gmail.com>
2025-03-27 18:58:47 +0600

    Add files via upload

---
+++ vowel count.c
@@ -0,0 +1,23 @@
+//count number of vowel
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    int i; char str[100];
+    printf("Enter string: ");
+    gets(str);
+    printf("Number of vowel: ");
+    int count=0;
+    for(i=0; str[i]!='\0';i++)
+    {
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
+            {count++;}
+
+    }
+    printf("%d",count);
+
+
+
+    return 0;
+}
